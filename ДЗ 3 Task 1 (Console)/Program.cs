44d7958568913ff71206256361539c9cad1fe852
YaroslavKNU2024 <86433715+YaroslavKNU2024@users.noreply.github.com>
2021-10-06 21:17:04 +0300

    Add files via upload

---
+++ ДЗ 3 Task 1 (Console)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ДЗ_3_Task_1__Console_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
