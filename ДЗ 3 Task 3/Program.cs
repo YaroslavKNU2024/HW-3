f2f494780dcf184d67d63dce4f720eb6cb5a585f
YaroslavKNU2024 <86433715+YaroslavKNU2024@users.noreply.github.com>
2021-10-06 21:27:37 +0300

    Add files via upload

---
+++ ДЗ 3 Task 3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Converter {
+    public Converter(double dollar, double euro) {
+        this.dollar = dollar;
+        this.euro = euro;
+    }
+    double dollar, euro;
+
+    public double dollar2hrv(double d) {
+        return d * dollar;
+    }
+    public double euro2hrv(double e) {
+        return e * euro;
+    }
+    public double hrv2dollar(double h) {
+        return h / dollar;
+    }
+    public double hrv2euro(double h) {
+        return h / euro;
+    }
+
+}
+namespace ДЗ_2__Task_3_
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter C = new Converter(28, 32.5);
+            Console.WriteLine(C.dollar2hrv(5));
+            Console.WriteLine(C.euro2hrv(7));
+            Console.WriteLine(C.hrv2euro(137));
+        }
+    }
+}
