f822b9558d95a15311e0d31c910bb4babd7e1a9d
YaroslavKNU2024 <86433715+YaroslavKNU2024@users.noreply.github.com>
2021-10-06 21:12:11 +0300

    Add files via upload

---
+++ ДЗ 3 task 1 (Windows Forms Application)/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ДЗ_2_task_1__Windows_Forms_Application_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "Hello World!";
+        }
+    }
+}
